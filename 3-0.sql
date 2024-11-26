958d5bc34b70b1ba6a5a937cb4818bc248e17a27
SaberLS <patryk.platek19@gmail.com>
2024-11-26 15:54:20 +0100

    exercises setup

---
+++ 3-0.sql
@@ -0,0 +1,101 @@
+USE Northwind
+
+-- 1. Wybierz nazwy i ceny produktów (baza northwind) o cenie jednostkowej pomiędzy 20.00 a 30.00, dla każdego produktu podaj dane adresowe dostawcy
+
+
+-- 2. Wybierz nazwy produktów oraz inf. o stanie magazynu dla produktów dostarczanych przez firmę ‘Tokyo Traders’
+
+
+-- 3. Czy są jacyś klienci którzy nie złożyli żadnego zamówienia w 1997 roku, jeśli tak TO pokaż ich dane adresowe
+
+
+-- 4. Wybierz nazwy i numery telefonów dostawców, dostarczających produkty, których aktualnie nie  ma w magazynie.
+
+
+-- 5. Wybierz zamówienia złożone w marcu 1997. Dla każdego takiego zamówienia wyświetl jego numer, datę złożenia zamówienia oraz nazwę i numer telefonu klienta
+
+
+-----------------------------------------------------------------------------
+
+-- 1. Napisz polecenie, które wyświetla listę dzieci będących członkami biblioteki (baza library). Interesuje nas imię, nazwisko i data urodzenia dziecka.
+
+
+-- 2. Napisz polecenie, które podaje tytuły aktualnie wypożyczonych książek
+
+
+-- 3. Podaj informacje o karach zapłaconych za przetrzymywanie książki o tytule ‘Tao Teh King’.  Interesuje nas data oddania książki, ile dni była przetrzymywana i jaką zapłacono karę
+
+
+-- 4. Napisz polecenie które podaje listę książek (mumery ISBN) zarezerwowanych przez osobę o nazwisku: Stephen A. Graff
+
+------------------------------------------------
+
+-- 1. Wybierz nazwy i ceny produktów (baza northwind) o cenie jednostkowej pomiędzy 20.00 a 30.00, dla każdego produktu podaj dane adresowe dostawcy, interesują nas tylko produkty z kategorii ‘Meat/Poultry’
+
+
+-- 2. Wybierz nazwy i ceny produktów z kategorii ‘Confections’ dla każdego produktu podaj nazwę dostawcy.
+
+
+-- 3. Dla każdego klienta podaj liczbę złożonych przez niego zamówień. Zbiór wynikowy powinien zawierać nazwę klienta, oraz liczbę zamówień
+
+
+-- 4. Dla każdego klienta podaj liczbę złożonych przez niego zamówień w marcu 1997r
+
+--------------
+
+
+
+
+-- 1. Który ze spedytorów był najaktywniejszy w 1997 roku, podaj nazwę tego spedytora
+
+
+-- 2. Dla każdego zamówienia podaj wartość zamówionych produktów. Zbiór wynikowy powinien zawierać nr zamówienia, datę zamówienia, nazwę klienta oraz wartość zamówionych produktów
+
+
+-- 3. Dla każdego zamówienia podaj jego pełną wartość (wliczając opłatę za przesyłkę). Zbiór wynikowy powinien zawierać nr zamówienia, datę zamówienia, nazwę klienta oraz pełną wartość zamówienia
+
+
+----------------------------------------------------
+
+
+-- 1. Wybierz nazwy i numery telefonów klientów, którzy kupowali produkty  z kategorii ‘Confections’
+
+
+-- 2. Wybierz nazwy i numery telefonów klientów, którzy nie kupowali produktów z kategorii ‘Confections’
+
+
+-- 3. Wybierz nazwy i numery telefonów klientów, którzy w 1997r nie kupowali produktów z kategorii ‘Confections’
+
+
+
+----------------------------------------------------
+
+-- 1. Napisz polecenie, które wyświetla listę dzieci będących członkami biblioteki
+-- (baza library). Interesuje nas imię, nazwisko, data urodzenia dziecka i adres zamieszkania dziecka.
+
+
+-- 2. Napisz polecenie, które wyświetla listę dzieci będących członkami biblioteki 
+--(baza library). Interesuje nas imię, nazwisko, data urodzenia dziecka, adres zamieszkania dziecka oraz imię i nazwisko rodzica.
+
+----------------------------------------------------
+
+
+-- 1. Napisz polecenie, które wyświetla pracowników oraz ich podwładnych
+-- (baza northwind)
+
+-- 2. Napisz polecenie, które wyświetla pracowników, którzy nie mają podwładnych
+-- (baza northwind)
+
+-- 3. Napisz polecenie, które wyświetla pracowników, którzy mają podwładnych
+-- (baza northwind)
+
+
+----------------------------------------------------
+
+
+-- 1. Podaj listę członków biblioteki mieszkających w Arizonie
+
+-- (AZ) mają  więcej niż dwoje dzieci zapisanych do biblioteki
+
+-- 2. Podaj listę członków biblioteki mieszkających w Arizonie (AZ) którzy mają  więcej niż dwoje dzieci zapisanych do biblioteki 
+-- oraz takich którzy mieszkają w Kaliforni (CA) i mają więcej niż troje dzieci zapisanych do biblioteki
